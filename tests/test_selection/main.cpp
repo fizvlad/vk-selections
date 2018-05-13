3264bc12c0229b10cd3863fd8d30ffaacbc95e05
Fizvlad <fizvlad@mail.ru>
2018-05-13 15:12:36 +0300

    edited test

--- tests/test_selection/main.cpp
+++ tests/test_selection/main.cpp
@@ -21,12 +21,12 @@ int main() {
     cout << "Token: " << token2[0] << token2[1] << token2[2] << token2[3] << "..." << endl << endl;
 
     Unit habr = Unit("habr", token);
-    Unit kinopoisk = Unit("kinopoisk", token);
+    Unit music = Unit("exclusive_muzic", token); // Largest public takes 3 minute to load ._ .
     Unit stepik = Unit("rustepik", token);
     Unit Filyus_Bot = Unit(283177744, true, token);
     Unit buerak = Unit("buerak_spring", token);
     cout << habr << endl
-         << kinopoisk << endl
+         << music << endl
          << stepik << endl
          << Filyus_Bot << endl
          << buerak << endl;
@@ -35,9 +35,13 @@ int main() {
     Selection test = Filyus_Bot.friends(token2);
     test.saveAs("result_Not_Filyus_friends");
 
-    // Works fine. Requires much time
-    Selection test2 = kinopoisk.members(token);
-    test2.saveAs("result_Kinopoisk_subscribers");
+    // Works fine. Requires time
+    Selection test2 = habr.members(token);
+    test2.saveAs("result_Habrahabr_subscribers");
+
+    // Works fine. Requires lot of time
+    //Selection test3 = music.members(token);
+    //test3.saveAs("result_Music_subscribers");
 
     return 0;
 }
